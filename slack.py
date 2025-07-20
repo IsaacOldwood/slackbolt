c66509dcc1f440527c22bec9bc94b4df37da3442
Isaac Oldwood <Isaac.Oldwood@gmail.com>
2025-07-20 11:35:44 +0100

    Event handler for clickup emoji

--- slack.py
+++ slack.py
@@ -4,11 +4,17 @@ from slack_bolt import App
 
 logging.basicConfig(level=logging.DEBUG)
 
-# export SLACK_SIGNING_SECRET=***
-# export SLACK_BOT_TOKEN=xoxb-***
-app = App(token=config("SLACK_BOT_TOKEN"))
+app = App(
+    token=config("SLACK_BOT_TOKEN"), signing_secret=config("SLACK_SIGNING_SECRET")
+)
 
-# Add functionality here
+@app.event("reaction_added")
+def handle_reaction_added_events(body, logger):
+    emoji = body.get('event', {}).get('reaction')
+    if not (emoji == 'clickup'):
+        return
+    else:
+        logger.debug(body)
 
 if __name__ == "__main__":
     app.start(3000)  # POST http://localhost:3000/slack/events
